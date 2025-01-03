bdf5d576c4405cc5060b22b5cc8043876d77571c
anand9589 <anand9589@gmail.com>
2025-01-02 20:31:43 -0500

    first check i9n

---
+++ LeetcodeProblems/Leetcode2025.Tests/Leetcode2025Tests.cs
@@ -0,0 +1,11 @@
+namespace Leetcode2025.Tests
+{
+    [TestClass]
+    public class Leetcode2025Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ LeetcodeProblems/Leetcode2025/Leetcode2025.cs
@@ -0,0 +1,41 @@
+﻿namespace Leetcode2025
+{
+    public class Leetcode2025
+    {
+
+
+        #region 2559. Count Vowel Strings in Ranges
+        public int[] VowelStrings(string[] words, int[][] queries)
+        {
+            HashSet<char> vowels = new HashSet<char>() { 'a', 'e', 'i', 'o', 'u' };
+            int[] preCount = new int[words.Length];
+            bool[] vowelString = new bool[words.Length];
+            int initialCount = 0;
+            for (int i = 0; i < words.Length; i++)
+            {
+                char first = words[i][0];
+                char last = words[i][words[i].Length - 1];
+                if (vowels.Contains(first) && vowels.Contains(last))
+                {
+                    initialCount++;
+                    vowelString[i] = true;
+                }
+                preCount[i] = initialCount;
+            }
+
+            int[] result = new int[queries.Length];
+
+            for (int i = 0; i < queries.Length; i++)
+            {
+                result[i] = preCount[queries[i][1]] - preCount[queries[i][0]];
+                if (vowelString[queries[i][0]])
+                {
+                    result[i] += 1;
+                }
+            }
+
+            return result;
+        }
+        #endregion
+    }
+}

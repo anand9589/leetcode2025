4675db44ee93f2c690752e51b56b80438a52ddb2
anand9589 <anand9589@gmail.com>
2025-08-04 17:54:23 -0400

    904. Fruit Into Baskets

--- LeetcodeProblems/Amazon2025/Solution.cs
+++ LeetcodeProblems/Amazon2025/Solution.cs
@@ -1,4 +1,6 @@
-﻿namespace Amazon2025
+﻿using System.Runtime.CompilerServices;
+
+namespace Amazon2025
 {
     public class Solution
     {
@@ -150,6 +152,268 @@
                 lst = temp;
                 result.Add(new List<int>(lst));
             }
+
+            return result;
+        }
+        #endregion
+
+        #region 904. Fruit Into Baskets
+        public int TotalFruit(int[] fruits)
+        {
+            int result = 1;
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            int startIndex = 0;
+            int currIndex = 1;
+            map.Add(fruits[0], 1);
+
+            while (currIndex < fruits.Length)
+            {
+                if (!map.ContainsKey(fruits[currIndex]))
+                {
+                    if (map.Count == 2)
+                    {
+                        result = Math.Max(result, currIndex - startIndex);
+                        while (map.Count == 2)
+                        {
+                            map[fruits[startIndex]]--;
+                            if (map[fruits[startIndex]] == 0)
+                            {
+                                map.Remove(fruits[startIndex]);
+                            }
+                            startIndex++;
+                        }
+                        map[fruits[currIndex]] = 1;
+                    }
+                    else
+                    {
+                        map[fruits[currIndex]] = 1;
+                    }
+                }
+                else
+                {
+                    map[fruits[currIndex]]++;
+                }
+
+                currIndex++;
+            }
+            return Math.Max(result, currIndex - startIndex);
+        }
+        #endregion
+
+        #region 2106. Maximum Fruits Harvested After at Most K Steps
+        public int MaxTotalFruits(int[][] fruits, int startPos, int k)
+        {
+            int result = 0;
+            int startPosFruit = 0;
+
+            List<int> left = new List<int>();
+            List<int> leftFruit = new List<int>();
+            List<int> right = new List<int>();
+            List<int> rightFruit = new List<int>();
+
+            foreach (int[] arr in fruits)
+            {
+                int diff = Math.Abs(startPos - arr[0]);
+                if (diff > k) continue;
+                if (arr[0] < startPos)
+                {
+                    left.Insert(0, diff);
+                    leftFruit.Insert(0, arr[1]);
+                }
+                else if (arr[0] == startPos)
+                {
+                    startPosFruit = arr[1];
+                }
+                else
+                {
+                    right.Add(diff);
+                    rightFruit.Add(arr[1]);
+                }
+            }
+
+            int[] leftPrefix = new int[left.Count + 1];
+
+            for (int i = 1; i <= left.Count; i++)
+            {
+                leftPrefix[i] = leftPrefix[i - 1] + leftFruit[i - 1];
+            }
+
+            int[] rightPrefix = new int[right.Count + 1];
+
+            for (int i = 1; i <= right.Count; i++)
+            {
+                rightPrefix[i] = rightPrefix[i - 1] + rightFruit[i - 1];
+            }
+
+            int kTemp = k;
+            int currResult = 0;
+            for (int i = 0; i < left.Count; i++)
+            {
+                kTemp = k;
+                currResult = leftPrefix[i + 1];
+
+                kTemp -= (2 * left[i]);
+                if (kTemp > 0)
+                {
+                    int ub = UpperBound(right, kTemp);
+
+                    currResult += rightPrefix[ub];
+                }
+                result = Math.Max(result, currResult);
+            }
+
+            for (int i = 0; i < right.Count; i++)
+            {
+                kTemp = k;
+                currResult = rightPrefix[i + 1];
+
+                kTemp -= (2 * right[i]);
+                if (kTemp > 0)
+                {
+                    int ub = UpperBound(left, kTemp);
+
+                    currResult += leftPrefix[ub];
+                }
+                result = Math.Max(result, currResult);
+            }
+            return result + startPosFruit;
+        }
+
+        private int UpperBound(List<int> arr, int k)
+        {
+
+            int left = 0;
+            int right = arr.Count;
+            int result = right;
+
+
+            while (left < right)
+            {
+                int mid = (left + right) / 2;
+
+                if (arr[mid] <= k)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    result = mid;
+                    right = mid - 1;
+                }
+            }
+
+            return result;
+
+        }
+        #endregion
+
+        #region 2561. Rearranging Fruits
+        public long MinCost(int[] basket1, int[] basket2)
+        {
+            long result = 0;
+            Dictionary<int, int> freqMap = new Dictionary<int, int>();
+            int min = int.MaxValue;
+            for (int i = 0; i < basket1.Length; i++)
+            {
+                int currMin = Math.Min(basket1[i], basket2[i]);
+                min = Math.Min(min, currMin);
+                if (!freqMap.ContainsKey(basket1[i]))
+                {
+                    freqMap.Add(basket1[i], 0);
+                }
+                if (!freqMap.ContainsKey(basket2[i]))
+                {
+                    freqMap.Add(basket2[i], 0);
+                }
+
+                freqMap[basket1[i]]++;
+                freqMap[basket2[i]]--;
+            }
+            List<int> lst = new List<int>();
+            foreach (var key in freqMap.Keys)
+            {
+                if (freqMap[key] == 0) continue;
+                if (freqMap[key] % 2 == 1) return -1;
+
+                for (int i = 0; i < Math.Abs(freqMap[key]) / 2; i++)
+                {
+                    lst.Add(key);
+                }
+            }
+            lst.Sort();
+
+            for (int i = 0; i < lst.Count / 2; i++)
+            {
+                result += lst[i];
+            }
+            return result;
+        }
+        public long MinCost25611(int[] basket1, int[] basket2)
+        {
+            Array.Sort(basket1);
+            Array.Sort(basket2);
+
+
+            Dictionary<int, int> basket1Extra = new Dictionary<int, int>();
+            Dictionary<int, int> basket2Extra = new Dictionary<int, int>();
+            for (int i = 0; i < basket1.Length; i++)
+            {
+                if (basket1[i] == basket2[i]) continue;
+                if (!basket1Extra.ContainsKey(basket1[i]))
+                {
+                    basket1Extra.Add(basket1[i], 0);
+                }
+                basket1Extra[basket1[i]]++;
+                if (!basket2Extra.ContainsKey(basket2[i]))
+                {
+                    basket2Extra.Add(basket2[i], 0);
+                }
+
+                basket2Extra[basket1[i]]++;
+            }
+            if (basket1Extra.Count == 0 && basket2Extra.Count == 0) return 0;
+
+            long result = 0;
+
+            while (basket1Extra.Count > 1 && basket2Extra.Count > 1)
+            {
+                int basket1First = basket1Extra.Keys.First();
+                int basket2Last = basket2Extra.Keys.Last();
+                if (basket1Extra[basket1First] == basket2Extra[basket2Last])
+                {
+                    result += (basket1First * basket1Extra[basket1First]);
+                    basket1Extra.Remove(basket1First);
+                    basket2Extra.Remove(basket2Last);
+                }
+                else if (basket1Extra[basket1First] < basket2Extra[basket2Last])
+                {
+                    result += (basket1First * basket1Extra[basket1First]);
+                    basket2Extra[basket2Last] -= basket1Extra[basket1First];
+                    basket1Extra.Remove(basket1First);
+                }
+                else
+                {
+                    result += (basket1First * basket2Extra[basket2Last]);
+                    basket1Extra[basket1First] -= basket2Extra[basket2Last];
+                    basket2Extra.Remove(basket2Last);
+                }
+            }
+
+            if (basket1Extra.Count == 1 && basket2Extra.Count == 1)
+            {
+                int key1 = basket1Extra.Keys.First();
+                int key2 = basket2Extra.Keys.First();
+
+                if (basket1Extra[key1] % 2 == 1 || basket2Extra[key2] % 2 == 1 || basket1Extra[key1] != basket2Extra[key2]) { return -1; }
+
+                int val = basket1Extra[key1] / 2;
+
+                int min = Math.Min(key1, key2);
+
+                result += (min * val);
+            }
+
+
 
             return result;
         }
--- LeetcodeProblems/Leetcode2025.Tests/Amazon2025Tests.cs
+++ LeetcodeProblems/Leetcode2025.Tests/Amazon2025Tests.cs
@@ -1,4 +1,5 @@
 using Amazon2025;
+using Common;
 
 namespace Leetcode2025.Tests;
 
@@ -25,6 +26,55 @@ public class Amazon2025Tests
     {
         int numRows = 6;
         var k = solution.Generate(numRows);
+    }
+    #endregion
+
+
+    #region 904. Fruit Into Baskets
+
+
+    [TestMethod]
+    public void TotalFruitTest()
+    {
+        int[] fruits = { 0, 1, 2, 2 };
+        var k = solution.TotalFruit(fruits);
+    }
+    #endregion
+
+    #region 2106. Maximum Fruits Harvested After at Most K Steps
+
+    [TestMethod]
+    public void MaxTotalFruitsTest()
+    {
+        string s = "[[2,8],[6,3],[8,6]]";
+        int[][] arr = Helper.GetMultiDimensionalArrayBasedOnString(s);
+        int startPos = 5;
+        int k = 4;
+
+        var t = solution.MaxTotalFruits(arr, startPos, k);
+    }
+
+    [TestMethod]
+    public void MaxTotalFruitsTest1()
+    {
+        string s = "[[0,9],[4,1],[5,7],[6,2],[7,4],[10,9]]";
+        int[][] arr = Helper.GetMultiDimensionalArrayBasedOnString(s);
+        int startPos = 5;
+        int k = 4;
+
+        var t = solution.MaxTotalFruits(arr, startPos, k);  
+    }
+    #endregion
+
+
+    #region 2561. Rearranging Fruits
+
+    [TestMethod]
+    public void MinCostTest()
+    {
+        int[] basket1 = {4,2,2,2};
+        int[] basket2 = { 1, 4, 1, 2 };
+        var k = solution.MinCost(basket1, basket2);
     }
     #endregion
 }
